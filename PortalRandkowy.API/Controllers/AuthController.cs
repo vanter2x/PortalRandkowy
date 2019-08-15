a19ad60a9234a4c02013f62cd17d8b2dfaa2bc72
Sławomir Kowal <vanter2x@gmail.com>
2019-08-15 19:45:39 +0200

    Dodanie dto

---
+++ PortalRandkowy.API/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PortalRandkowy.API.Data;
+using PortalRandkowy.API.Dtos;
+using PortalRandkowy.API.Models;
+
+namespace PortalRandkowy.API.Controllers {
+    [Route ("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase {
+        private readonly IAuthRepository _repository;
+
+        public AuthController (IAuthRepository repository) {
+            _repository = repository;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegosterDto)
+        {
+            userForRegosterDto.Username = userForRegosterDto.Username.ToLower();
+
+            if(await _repository.UserExist(userForRegosterDto.Username))
+                return BadRequest("Użytkownik już istnieje.");
+
+            var userToCreate = new User{
+                Username = userForRegosterDto.Username
+            };
+            var createdUser = await _repository.Register(userToCreate, userForRegosterDto.Password);
+
+            return StatusCode(201);
+        }
+    }
+}
