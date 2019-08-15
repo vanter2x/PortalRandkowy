1acd1fd529e91d199ea3c675458f87d6c6d36b8c
Sławomir Kowal <vanter2x@gmail.com>
2019-08-15 21:27:29 +0200

    dod obslugi tokena

--- PortalRandkowy.API/Controllers/AuthController.cs
+++ PortalRandkowy.API/Controllers/AuthController.cs
@@ -1,17 +1,25 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using PortalRandkowy.API.Data;
 using PortalRandkowy.API.Dtos;
 using PortalRandkowy.API.Models;
 
 namespace PortalRandkowy.API.Controllers {
     [Route ("api/[controller]")]
-    //[ApiController]
+    [ApiController]
     public class AuthController : ControllerBase {
         private readonly IAuthRepository _repository;
+        private readonly IConfiguration _config;
 
-        public AuthController (IAuthRepository repository) {
+        public AuthController (IAuthRepository repository, IConfiguration config) {
             _repository = repository;
+            _config = config;
         }
 
         [HttpPost("register")]
@@ -28,6 +36,38 @@ namespace PortalRandkowy.API.Controllers {
             var createdUser = await _repository.Register(userToCreate, userForRegosterDto.Password);
 
             return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            var userFromRepo = await _repository.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+
+            if(userFromRepo == null)
+             return Unauthorized();
+
+            //create token
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.Username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key,SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddHours(12),
+                SigningCredentials = creds
+            };
+
+            var tokenHendler = new JwtSecurityTokenHandler();
+            var token = tokenHendler.CreateToken(tokenDescriptor);
+
+            return Ok(new { token = tokenHendler.WriteToken(token)});
         }
     }
 }
