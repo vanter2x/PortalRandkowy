596482550936d71779d168d45313a1b4521692fe
Sławomir Kowal <vanter2x@gmail.com>
2019-08-16 19:57:52 +0200

    Użycie uwierzytelniania

--- PortalRandkowy.API/Controllers/ValuesController.cs
+++ PortalRandkowy.API/Controllers/ValuesController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PortalRandkowy.API.Data;
@@ -8,6 +9,7 @@ using PortalRandkowy.API.Models;
 
 namespace PortalRandkowy.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -29,6 +31,7 @@ namespace PortalRandkowy.API.Controllers
         }
 
         // GET api/values/5
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
