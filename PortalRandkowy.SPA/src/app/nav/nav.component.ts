5be217f6ba83442a19e959fdf772dd8fcacb5f07
Sławomir Kowal <vanter2x@gmail.com>
2019-08-18 15:52:32 +0200

    validacja formularza

--- PortalRandkowy.SPA/src/app/nav/nav.component.ts
+++ PortalRandkowy.SPA/src/app/nav/nav.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
+  model: any = {};
   constructor() { }
 
   ngOnInit() {
   }
 
+  login(){
+    console.log(this.model);
+  }
 }
